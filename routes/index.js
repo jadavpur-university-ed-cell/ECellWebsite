9d7f1123ae6a7e0c68caab33d54e51024ad22e28
Sk Shahnawaz <skshahnawaz2909@gmail.com>
2023-03-22 06:10:43 +0530

    major skeleton done

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,39 @@ router.get("/startups", function (req, res) {
     res.render("pages/startups", {});
 });
 
+// events page
+router.get("/events", function (req, res) {
+    res.render("pages/events", {});
+});
+
+// contact page
+router.get("/contact", function (req, res) {
+    res.render("pages/contact", {});
+});
+
+// hult page
+router.get("/hult", function (req, res) {
+    res.render("pages/hult", {});
+});
+
+// eweekend page
+router.get("/eweekend", function (req, res) {
+    res.render("pages/eweekend", {});
+});
+
+// esummit page
+router.get("/esummit", function (req, res) {
+    res.render("pages/esummit", {});
+});
+// initiatives page
+router.get("/initiatives", function (req, res) {
+    res.render("pages/initiatives", {});
+});
+
+// join-us page
+router.get("/join-us", function (req, res) {
+    res.render("pages/join-us", {});
+});
+
+
 module.exports = router;
